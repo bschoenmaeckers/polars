e3f57c35c1e8ba89921b6e63fe0c384a7439c264
Bas Schoenmaeckers <b.schoenmaeckers@zanders.eu>
2024-12-02 16:10:43 +0100

    feat: make `PySQLContext` thread safe

--- crates/polars-python/src/sql.rs
+++ crates/polars-python/src/sql.rs
@@ -1,14 +1,15 @@
+use std::sync::Mutex;
+
 use polars::sql::SQLContext;
 use pyo3::prelude::*;
 
 use crate::error::PyPolarsErr;
 use crate::PyLazyFrame;
 
-#[pyclass(unsendable)]
+#[pyclass]
 #[repr(transparent)]
-#[derive(Clone)]
 pub struct PySQLContext {
-    pub context: SQLContext,
+    pub context: Mutex<SQLContext>,
 }
 
 #[pymethods]
@@ -22,27 +23,29 @@ impl PySQLContext {
     #[allow(clippy::new_without_default)]
     pub fn new() -> PySQLContext {
         PySQLContext {
-            context: SQLContext::new(),
+            context: Mutex::new(SQLContext::new()),
         }
     }
 
     pub fn execute(&mut self, query: &str) -> PyResult<PyLazyFrame> {
         Ok(self
             .context
+            .lock()
+            .unwrap()
             .execute(query)
             .map_err(PyPolarsErr::from)?
             .into())
     }
 
     pub fn get_tables(&self) -> PyResult<Vec<String>> {
-        Ok(self.context.get_tables())
+        Ok(self.context.lock().unwrap().get_tables())
     }
 
     pub fn register(&mut self, name: &str, lf: PyLazyFrame) {
-        self.context.register(name, lf.ldf)
+        self.context.lock().unwrap().register(name, lf.ldf)
     }
 
     pub fn unregister(&mut self, name: &str) {
-        self.context.unregister(name)
+        self.context.lock().unwrap().unregister(name)
     }
 }
